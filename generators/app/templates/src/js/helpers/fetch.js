9d5735ccce4a9d2f5014c0ba41aee0f04f10d95b
Thibault Maekelbergh <thibault.maekelbergh@student.howest.be>
2016-11-04 20:20:32 +0100

    rest operator needs to be enclosed

--- generators/app/templates/src/js/helpers/fetch.js
+++ generators/app/templates/src/js/helpers/fetch.js
@@ -1,6 +1,6 @@
 import {createFetch, auth, parse} from 'http-client';
 
-export default fetch = ...args => createFetch(
+export default fetch = (...args) => createFetch(
   <% if(jwt) { %>auth(`Bearer ${token}`),<% } %>
   accept('application/json'),
   parse('json'),
